fd7ff77fdc162e6ae6deaeabfb3a19203672b640
VictorGM01 <victormarques8801@gmail.com>
2021-11-02 18:25:02 -0300

    implementa função que encaminha o e-mail automaticamente

--- automacao/automacao_apod_nasa.py
+++ automacao/automacao_apod_nasa.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from datetime import datetime
 import smtplib
-import email.message
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from credencial import SENHA
 
 
 navegador = webdriver.Chrome()
@@ -24,27 +27,41 @@ def extrai_descricao_da_foto():
 
 def encaminha_email():
     corpo_email = f"""
-    <center>
-        <h1>
-            Foto de Hoje - NASA
-        </h1>
-    </center>
-    <p>
-        {data.day} de novembro de {data.year}
-    </p>
-    <p>
-        <img src="{url_imagem}">
-    </p>
-    <p>
-        {descricao}
-    </p>
+    <html>
+        <head></head>
+        <body>
+            <center>
+                <h1>
+                    Foto de Hoje - NASA
+                </h1>
+            </center>
+            <p>
+                {data.day} de novembro de {data.year}
+            </p>
+            <p>
+                <img src="{url_imagem}">
+            </p>
+            <p>
+                {descricao}
+            </p>
+        </body>
+    </html>
     """
 
-    msg = email.message.Message()
+    msg = MIMEMultipart()
     msg['Subject'] = 'Astronomia - Foto do Dia'
     msg['From'] = 'victormarques8801@gmail.com'
+    msg['To'] = 'victormarques8801@gmail.com'
+    msg.attach(MIMEText(corpo_email, 'html'))
 
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.ehlo()
+    s.starttls()
+    s.login(msg['From'], SENHA)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
 
 
 extrai_foto_do_site()
 extrai_descricao_da_foto()
+encaminha_email()
